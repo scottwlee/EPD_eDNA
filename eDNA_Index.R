fe01f4394183d09be8400462868098452d6235d2
scottwlee <119664753+scottwlee@users.noreply.github.com>
2024-05-07 18:20:47 +0800

    Update eDNA_Index.R

--- eDNA_Index.R
+++ eDNA_Index.R
@@ -1,7 +1,4 @@
-### Reference: Kelly, R. P., Shelton, A. O. & Gallego, R. Understanding PCR processes to draw meaningful conclusions from environmental DNA studies. Sci. Rep. 9, 12133–14 (2019).
-### URL: https://github.com/invertdna/eDNA_Process_Simulations/blob/master/FunctionsForSimulationPaper.R
-
-#function to calculate eDNA index
+##### Function to calculate eDNA index
 eDNAINDEX <- function(x) { #where x is a dataframe with taxa/OTUs/etc in rows, and samples in columns
   rowMax <- function(x){apply(x, MARGIN = 1, FUN = max)}
   temp <- sweep(x, MARGIN = 2, STATS = colSums(x), FUN = "/") #create proportion for each taxon/OTU within each sample
